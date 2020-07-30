61e38674383b14b65178d2a01479651f23893cfa
Martin Andrew <MartinAndrew2@JohnDeere.com>
2020-07-30 08:39:01 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+let cards = ["queen","queen","king","king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
 
-console.log("User flipped "+ cardOne);
-console.log("User flipped "+ cardTwo);
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped "+cardOne);
+console.log("User flipped "+cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!')
+	} else {
+		alert('Sorry, try again.')
+	}
+}
