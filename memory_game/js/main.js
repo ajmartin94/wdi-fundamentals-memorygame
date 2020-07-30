d1c8a7412dbd734ae23d4884f25214462772d008
Martin Andrew <MartinAndrew2@JohnDeere.com>
2020-07-30 17:01:06 -0500

    finalized the simple game with scoreboard and reset button

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,25 +23,63 @@ let cards = [
 
 let cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert('You found a match!')
-	} else {
-		alert('Sorry, try again.')
+function createBoard() {
+	for (let i=0; i < cards.length; i++) {
+		let cardElement = document.createElement("img");
+		cardElement.setAttribute("src","images/back.png");
+		cardElement.setAttribute("data-id",i);
+		cardElement.addEventListener("click",flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped "+cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+function resetBoard() {
+	let currentBoard = document.getElementsByTagName("img");
+	//console.log(currentBoard.length)
+	for (let i=currentBoard.length-1; i >= 0 ; i--) {
+		//console.log(currentBoard[i].getAttribute("src"))
+		currentBoard[i].remove();
+	}
+	createBoard();
+	cardsInPlay = [];
+}
+
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+		currentScore = currentScore + 1;
+		userScoreEl.innerText = currentScore;
+	} else {
+		alert('Sorry, try again.');
+		currentScore = currentScore - 1;
+		userScoreEl.innerText = currentScore;
+	}
+}
+
+function flipCard() {
+	let cardId = this.getAttribute("data-id");
+	//console.log("User flipped "+cards[cardId].rank);
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
 
 	cardsInPlay.push(cards[cardId].rank);
 
+	this.setAttribute("src",cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
+		console.log(currentScore);
 	}
 }
 
-flipCard(0);
-flipCard(2);
+//Set the initial score of 0 and establish the memory for score within the script
+let initialScore = 0;
+let userScoreEl = document.getElementById("user-score");
+userScoreEl.innerText = initialScore;
+let currentScore = initialScore;
+
+//create the game board
+createBoard();
+
+//reset the game board when the button is pressed
+document.getElementById("reset-button").addEventListener("click",resetBoard);
